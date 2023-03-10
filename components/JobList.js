aa0f92ceded48e3e60731fce45ab6777073e3623
ianfshirley <ianfshirley88@gmail.com>
2023-03-10 13:07:56 -0800

    mapping through jobs in DB on JobList component

--- components/JobList.js
+++ components/JobList.js
@@ -1,14 +1,20 @@
 import useResource from '../hooks/useResource';
 
 
-export default function JobList () {
-  
+export default function JobList() {
+
   const { resources } = useResource();
 
   return (
     <>
-      <p>{resources[0].title}</p>
-      <p>{resources[0].company}</p>
+      {resources && resources.map((job) => {
+        return (
+          <div key={job.id} job={job}>
+          <p>{job.title}</p>
+          <p>{job.company}</p>
+        </div>
+        )
+      })}
     </>
   )
 }
