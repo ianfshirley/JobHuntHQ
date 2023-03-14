42f148502e15a2826ccb3d5f859300edf21c81d3
ianfshirley <ianfshirley88@gmail.com>
2023-03-13 22:26:42 -0700

    moved signout button into header and added styling

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -12,7 +12,7 @@ export default function Dashboard() {
     <div className=" flex flex-col justify-around text-center content-center bg-gray-200 min-h-screen">
       <Header />
       <h1 className="bg-rose-200 text-3xl text-cyan-900">Dashboard Page</h1>
-      <div className='bg-green-300'>
+      {/* <div className='bg-green-300'>
         <h3>You are logged in as {session.user.name}</h3>
         <button
           className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-auto mb-2 w-32"
@@ -21,7 +21,7 @@ export default function Dashboard() {
         >
           Sign Out
         </button>
-      </div>
+      </div> */}
       <AddJobForm />
       <JobList />
     </div>
--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,31 @@
 import Image from "next/image"
 import JHQ from 'public/JHQ.png'
+import JobHuntHQ from 'public/JobHuntHQ.png'
+import { useSession, signOut } from "next-auth/react"
 
 
 export default function Header() {
+
+  const { data: session } = useSession()
+
   return (
     <>
-      <header>
+      <header className="flex justify-between bg-green-300">
         <Image
-          src={JHQ}
-          alt='JHQ Logo'
+          className="pl-6"
+          src={JobHuntHQ}
+          alt='JobHuntHQ Logo'
         />
+        <div className='flex justify-evenly items-center'>
+          <h3 className="px-5">You are logged in as {session.user.name}</h3>
+          <button
+            className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-4 w-32"
+            type="button"
+            onClick={() => signOut()}
+          >
+            Sign Out
+          </button>
+        </div>
       </header>
     </>
   )
