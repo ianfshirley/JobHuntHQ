57e0e0afeb8152a7ff48b826110d73038f0e5aac
ianfshirley <ianfshirley88@gmail.com>
2023-03-14 15:18:49 -0700

    added styling to update job modal and changed the notes section to size-adjustable textarea

--- components/UpdateJobModal.js
+++ components/UpdateJobModal.js
@@ -69,53 +69,68 @@ export default function UpdateJobModal(props) {
         isOpen={props.isModalOpen}
         onRequestClose={props.toggleModal}
         contentLabel="Example Modal"
-        overlayClassName="overlay"
+        // overlayClassName="overlay"
         ariaHideApp={false}
-        className=""
+        className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'
       >
-        <h1 className="text-3xl">Update Job Form Coming Soon!</h1>
-        <button
-          className="absolute px-2 text-lg font-bold text-center transition-all duration-300 border border-white rounded-md top-4 right-4 hover:text-red-500 hover:border-red-500 text-violet-700"
-          onClick={props.toggleModal}
-        >
-          X
-        </button>
 
-        {/* <h3>{props.job.title} at {props.job.company}</h3>
-        <p>Date Applied: {props.job.date_applied}</p>
-        <p>Method of Application: {props.job.method}</p>
-        <p>Cover Letter? {props.job.cover_letter}</p>
-        <p>Referral? {props.job.referral}</p>
-        <p>Notes:  {props.job.notes}</p>
-        <p>First Interview?  {props.job.first}</p> */}
+        <div className="relative w-auto my-6 mx-auto max-w-3xl">
 
-        <form
-          onSubmit={(e) => handleUpdateJob(props.job.id, e)}
-          className="flex w-6/12 p-6 mx-auto my-16 bg-blue-800/50 border border-blue-900 h-3/4 place-contents-center"
-        >
-          <fieldset className='w-11/12 place-contents-center'>
+          {/* Modal Content: */}
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
 
-            <h3 className='text-white p-1'>Job Title</h3>
-            <input type='text' name='title' id='title' className='content-center mx-2 my-2 w-6/12 p-0.5' defaultValue={props.job.title} />
+            {/* Modal Header: */}
+            <div className="text-center p-5 border-b border-solid border-slate-200 rounded-t">
+              <h1 className="text-3xl">Update Job</h1>
+              <button
+                className="absolute px-2 text-lg font-bold text-center transition-all duration-300 border border-white rounded-md top-4 right-4 hover:text-red-500 hover:border-red-500 text-violet-700"
+                onClick={props.toggleModal}
+              >
+                X
+              </button>
+            </div>
 
-            <h3 className='text-white p-1'>Company</h3>
-            <input type='text' name='company' id='company' className='content-center mx-2 my-2 w-6/12 p-0.5' defaultValue={props.job.company} />
+            {/* Modal Body: */}
+            <div className="relative p-6 flex-auto">
+
+              <p>First Interview?  {props.job.first}</p>
+
+              <form
+                onSubmit={(e) => handleUpdateJob(props.job.id, e)}
+                className="flex mx-10 p-6 bg-blue-800 border border-blue-900 justify-center"
+              >
+                <fieldset className='w-11/12 flex flex-col items-center'>
+
+                  <h3 className='text-white p-1'>Job Title</h3>
+                  <input type='text' name='title' id='title' className='content-center mx-2 my-2 p-0.5' defaultValue={props.job.title} />
+
+                  <h3 className='text-white p-1'>Company</h3>
+                  <input type='text' name='company' id='company' className='content-center mx-2 my-2 w-6/12 p-0.5' defaultValue={props.job.company} />
 
 
-            <h3 className='text-white p-1'>Did you write a cover letter?</h3>
-            <input type='text' name='cover_letter' id='cover_letter' className='content-center mx-2 my-2 w-6/12 p-0.5' defaultValue={props.job.cover_letter}/>
+                  <h3 className='text-white p-1'>Did you write a cover letter?</h3>
+                  <input type='text' name='cover_letter' id='cover_letter' className='content-center mx-2 my-2 w-6/12 p-0.5' defaultValue={props.job.cover_letter} />
 
-            <h3 className='text-white'>Did you have a referral? If so, who were they?</h3>
-            <input type='text' name='referral' id='referral' className='content-center mx-2 my-2 w-6/12 p-0.5' defaultValue={props.job.referral} />
+                  <h3 className='text-white'>Did you have a referral? If so, who were they?</h3>
+                  <input type='text' name='referral' id='referral' className='content-center mx-2 my-2 p-0.5' defaultValue={props.job.referral} />
 
-            <h3 className='text-white p-1'>Notes</h3>
-            <input type='text' name='notes' id='notes' className='content-center mx-2 my-2 w-6/12 p-0.5' defaultValue={props.job.notes} />
+                  <h3 className='text-white p-1'>Notes</h3>
+                  <textarea
+                    className='content-center mx-2 my-2  p-0.5'
+                    name='notes'
+                    id='notes'
+                    defaultValue={props.job.notes}
+                    rows="1" // start with one row
+                    style={{ resize: 'vertical', minHeight: '50px' }} // set the minimum height and allow vertical resizing
+                  />
 
-            <button className='text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-mono rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 m-4'>Submit</button>
+                  <button className='text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-mono rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 m-4'>Save Changes</button>
 
-          </fieldset>
-        </form>
-
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </div>
       </Modal>
     </>
   )
