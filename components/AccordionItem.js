83192ab6fb4577a0afe03a55ea97b8817d617f02
ianfshirley <ianfshirley88@gmail.com>
2023-03-15 15:31:45 -0700

    rendering job list as accordion

---
+++ components/AccordionItem.js
@@ -0,0 +1,26 @@
+import { GoX, GoChevronDown } from 'react-icons/go';
+import { Collapse } from 'react-collapse';
+
+
+export default function AccordionItem({open, toggleAccordionItem, title, date_applied, method, cover_letter, referral, notes, first, second, third, rejected, offer}) {
+
+
+  return (
+    <div className="pt-[10px]">
+      <div
+      className="bg-white py-[25px] ox-[50px] flex justify-between items-center cursor-pointer"
+      onClick={toggleAccordionItem}
+      >
+        <p className="text-[22px] font-semibold">{title}</p>
+        <div className="text-[30px]">
+          {open ? <GoX /> : <GoChevronDown />}
+        </div>
+      </div>
+
+      <Collapse isOpened={open}>
+        <div className='bg-white px-[50px] pb-[20px]'>{date_applied}</div>
+      </Collapse>
+
+    </div>
+  )
+}
--- components/JobList.js
+++ components/JobList.js
@@ -1,9 +1,20 @@
 import useResource from '../hooks/useResource';
 import UpdateJobModal from "./UpdateJobModal"
 import React, { useState } from 'react';
+import AccordionItem from './AccordionItem';
 
 
 export default function JobList() {
+
+  // AccordionItem state:
+  const [open, setOpen] = useState(false);
+
+  const toggleAccordionItem = (index) => {
+    if (open === index) {
+      return setOpen(null)
+    }
+    setOpen(index)
+  }
 
   const { resources, deleteResource } = useResource();
 
@@ -24,39 +35,39 @@ export default function JobList() {
       deleteResource(job.id);
       console.log('Job deleted');
     } else {
-      console.log('Job not deleted'); 
+      console.log('Job not deleted');
     }
   }
 
   return (
     <div className='bg-blue-300'>
       <h3 className='text-2xl pb-2'>Job List</h3>
-      {resources && resources.map((job) => {
+      {resources && resources.map((job, index) => {
         return (
-          <div key={job.id} job={job}>
-            <p>{job.title} at {job.company}</p>
-            <button
-              className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-auto mb-2 w-32"
-              onClick={() => openUpdateModal(job)}
-            >
-              Update
-            </button>
-            <button
-              className="text-white bg-gradient-to-br from-pink-300 to-orange-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center  m-2 w-32"
-              onClick={() => handleDeleteJob(job)}
-            >
-              Delete
-            </button>
-
-          </div>
+          <AccordionItem
+            key={index}
+            open={index === open}
+            title={`${job.title} at ${job.company}`}
+            date_applied={job.date_applied}
+            method={job.method}
+            cover_letter={job.cover_letter}
+            referral={job.referral}
+            notes={job.notes}
+            first={job.first}
+            second={job.second}
+            third={job.third}
+            rejected={job.rejected}
+            offer={job.offer}
+            toggleAccordionItem={() => toggleAccordionItem(index)}
+          />
         )
       })}
       {selectedJob && (
         <UpdateJobModal
-        isModalOpen={isUpdateModalOpen}
-        toggleModal={toggleUpdateModal}
-        job={selectedJob}
-      />
+          isModalOpen={isUpdateModalOpen}
+          toggleModal={toggleUpdateModal}
+          job={selectedJob}
+        />
       )}
     </div>
   )
