a86250d15849d640b05b766d1508346680d4baa7
ianfshirley <ianfshirley88@gmail.com>
2023-03-30 13:44:55 -0700

    added alert if user did not select method of application on add job form

--- components/AddJobModal.js
+++ components/AddJobModal.js
@@ -30,6 +30,12 @@ export default function AddJobModal(props) {
 
     e.preventDefault()
 
+    // Check if the user has selected a value for the dropdown menu
+    if (selectedValue === 'choose') {
+      alert('Please select an option for how you applied to the job.');
+      return;
+    }
+
     const originalDate = e.target.date_applied.value;
     const parsedDateArr = originalDate.split('/');
     const parsedDate = `${parsedDateArr[2]}-${parsedDateArr[0]}-${parsedDateArr[1]}`;
@@ -99,24 +105,24 @@ export default function AddJobModal(props) {
                 className="grid grid-cols-2 grid-rows-6 text-lg font-semibold bg-cornflower border-4 border-cream shadow-sm shadow-twilight items-center m-2 p-2"
               >
 
-                  <label className='flex justify-evenly items-center col-start-1 col-span-2 row-start-1 row-span-1'>
-                    Job Title
-                    <input type='text' name='title' id='title' className='content-center m-2 w-2/3 px-2 py-0.5' placeholder='ex: Software Developer' />
-                  </label>
+                <label className='flex justify-evenly items-center col-start-1 col-span-2 row-start-1 row-span-1'>
+                  Job Title
+                  <input type='text' name='title' id='title' className='content-center m-2 w-2/3 px-2 py-0.5' placeholder='ex: Software Developer' />
+                </label>
 
-                  <label className='flex justify-evenly items-center col-start-1 col-span-2 row-start-2 row-span-1'>
-                    Company
-                    <input type='text' name='company' id='company' className='content-center m-2 w-2/3 px-2 py-0.5' placeholder='ex: Code Fellows' />
-                  </label>
+                <label className='flex justify-evenly items-center col-start-1 col-span-2 row-start-2 row-span-1'>
+                  Company
+                  <input type='text' name='company' id='company' className='content-center m-2 w-2/3 px-2 py-0.5' placeholder='ex: Code Fellows' />
+                </label>
 
-                  <label className='flex whitespace-nowrap justify-evenly items-center col-span-1 row-start-3 row-span-1 ml-2'>
-                    Date Applied
-                    <DatePicker name='date_applied' id='date_applied' showIcon selected={startDate} onChange={(date) => setStartDate(date)} className="ml-4 text-center" />
-                  </label>
+                <label className='flex whitespace-nowrap justify-evenly items-center col-span-1 row-start-3 row-span-1 ml-2'>
+                  Date Applied
+                  <DatePicker name='date_applied' id='date_applied' showIcon selected={startDate} onChange={(date) => setStartDate(date)} className="ml-4 text-center" />
+                </label>
 
-                  <label className='flex justify-center items-center col-start-2 col-span-1 row-start-3 row-span-1'>
-                    How did you apply?
-                    <select
+                <label className='flex justify-center items-center col-start-2 col-span-1 row-start-3 row-span-1'>
+                  How did you apply?
+                  <select
                     value={selectedValue}
                     onChange={(e) => setSelectedValue(e.target.value)}
                     name='method'
@@ -128,41 +134,41 @@ export default function AddJobModal(props) {
                     <option value="otta">Otta</option>
                     <option value="company_website">Company Website</option>
                     <option value="other">Other</option>
-                    </select>
-                  </label>
+                  </select>
+                </label>
 
-                  <label className='flex justify-center items-center col-span-1 row-start-4 row-span-1'>
-                    Did you write a cover letter?
-                    <input
-                      className="mx-4 w-4 h-4 bg-cream border-dusk rounded"
-                      type="checkbox"
-                      checked={cover_letter}
-                      onChange={handleCoverLetterChange}
-                    />
-                  </label>
+                <label className='flex justify-center items-center col-span-1 row-start-4 row-span-1'>
+                  Did you write a cover letter?
+                  <input
+                    className="mx-4 w-4 h-4 bg-cream border-dusk rounded"
+                    type="checkbox"
+                    checked={cover_letter}
+                    onChange={handleCoverLetterChange}
+                  />
+                </label>
 
-                  <label className='flex justify-center items-center col-start-2 col-span-1 row-start-4 row-span-1'>
-                    Did you have a referral?
-                    <input
-                      className="mx-4 w-4 h-4 bg-cream border-dusk rounded"
-                      type="checkbox"
-                      checked={referral}
-                      onChange={handleReferralChange}
-                    />
-                  </label>
+                <label className='flex justify-center items-center col-start-2 col-span-1 row-start-4 row-span-1'>
+                  Did you have a referral?
+                  <input
+                    className="mx-4 w-4 h-4 bg-cream border-dusk rounded"
+                    type="checkbox"
+                    checked={referral}
+                    onChange={handleReferralChange}
+                  />
+                </label>
 
-                  <label className='flex justify-evenly items-center col-span-2 row-start-5 row-span-1'>
-                    Notes
-                    <textarea
-                      className='content-center mx-2 my-2 w-2/3 p-0.5'
-                      name='notes'
-                      id='notes'
-                      rows="1" // start with one row
-                      style={{ resize: 'vertical', minHeight: '50px' }} // set the minimum height and allow vertical resizing
-                    />
-                  </label>
+                <label className='flex justify-evenly items-center col-span-2 row-start-5 row-span-1'>
+                  Notes
+                  <textarea
+                    className='content-center mx-2 my-2 w-2/3 p-0.5'
+                    name='notes'
+                    id='notes'
+                    rows="1" // start with one row
+                    style={{ resize: 'vertical', minHeight: '50px' }} // set the minimum height and allow vertical resizing
+                  />
+                </label>
 
-                  <button className='col-span-2 row-start-6 row-span-1 text-white bg-gradient-to-r from-dusk via-twilight to-slate-700 hover:bg-gradient-to-br shadow-md shadow-twilight font-mono rounded-lg text-sm px-5 py-2.5 text-center mx-auto'>Add New Application</button>
+                <button className='col-span-2 row-start-6 row-span-1 text-white bg-gradient-to-r from-dusk via-twilight to-slate-700 hover:bg-gradient-to-br shadow-md shadow-twilight font-mono rounded-lg text-sm px-5 py-2.5 text-center mx-auto'>Add New Application</button>
 
 
               </form>
